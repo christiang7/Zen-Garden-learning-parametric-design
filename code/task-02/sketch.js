b8d7b4994e1ffc67d277e616c972bffcae1799e1
annameide <46717848+annameide@users.noreply.github.com>
2022-04-14 11:56:49 +0200

    sketch size re-definition
    re-definition of the canvas size by using variables

--- code/task-02/sketch.js
+++ code/task-02/sketch.js
@@ -1,13 +1,20 @@
+// festlegung später unveränderbarer variablen, d.h. die durchgehend KONSTANT bleiben
+const sketchHeight = 400;
+const sketchWidth = 400; 
+
 function preload(){
   // preload assets
 }
 
+// jetzt kann die canvasgrösse nicht über fixe werte, sondern über 
+// oben definierte variablen beschrieben werden. achtung: die syntax bleibt dabei gleich!
 function setup() {
-  createCanvas(400, 400, SVG);
+  // createCanvas(400, 400, SVG); <-- alte schreibweise
+  createCanvas(sketchWidth, sketchHeight, SVG);
 }
 
 function draw() {
-  background('blue');
+  background('yellow');
 }
 
 // comment your code anna
