6804c5d63e7c1992ad0361b2bccb36fd9d2b96a3
annameide <46717848+annameide@users.noreply.github.com>
2022-04-22 17:55:46 +0200

    Update sketch_conditions.js
    commented code

--- code/playground/sketch_conditions.js
+++ code/playground/sketch_conditions.js
@@ -1,68 +1,50 @@
+// declaration of variables with a global scope
 const sketchWidth = 400;
 const sketchHeight = 400;
-const blobSize = 30; 
-const padding = 20;
-const spacing = blobSize + 2 * padding;
-
-function preload(){
-  // preload assets
-}
+const size = 20;
 
 function setup() {
-  createCanvas(400, 400);
-}
+  // the visible drawing space (=canvas) is being generated
+  // createCanvas is a function with two parameters (here: width and height)
+  // createCanvas is a function that creates an object, the object is the visible drawing space (=canvas)
+  createCanvas(sketchWidth, sketchHeight);
 
-function draw() {
-  //background(100);
-  background('#faedb9');
-  //noLoop();
-  const columns = sketchWidth / blobSize;
-  const rows = sketchHeight / blobSize;
+  // by setting here these specific parameters, we overwrite the default values contained by the createCanvas function
+  background(255);
+  stroke(255);
+  fill(0);
   
-  for(let x = 0; x <= columns; x += 1) {
-    for(let y = 0; y <= rows; y += 1) {
-      
-      fill(random(220, 270), 213, 249); // syntax: random ((r),g,b) and r (red) is given a range to vary in 
-      noStroke();
-      //circle(x * spacing, y * spacing, circleSize);
-      blob(x * spacing, y * spacing);//(random(220, 270), 213, 249) 'red')
+  // here the constant variables for the upcoming grid creation are being declared
+  // the "columns" variable is a math operation NOT just one number
+  // the operation being variable "sketchWidth" divided by variable "size"
+  // the result of the devision operation is then being stored (=) in the "column" variable
+  const columns = sketchWidth / size;
+  const rows = sketchHeight / size;
+
+  // a for loop is being used for the repitition of a task
+  // here: the first for loop creates columns
+  // here: the second for loop creates rows 
+  for (let x = 0; x < columns; x += 1) { // draw shape (circle) on x achsis
+    for (let y = 0; y < rows; y += 1) { // draw shape (circle) on y achsis
+      const shapeNum = random(0,2); // random is given a range like 0,2, but the max. range value is NOT included, meaning it can not be reached
+      if (shapeNum < 1) {
+        circle(x * size + size / 2, y * size + size / 2, size); // coordinates like for the center of the circle can also be math operations NOT just numbers or variables
+      } else {
+        rect(x * size, y * size, size, size);
+      }
     }
   }
 }
 
-function blob(x,y) {
-  //fill(color);
-  //stroke(1);
-  beginShape();
-  curveVertex(x+51,y+20);
-  curveVertex(x+65,y+23);
-  curveVertex(x+77,y+23);
-  curveVertex(x+83,y+34);
-  curveVertex(x+88,y+45);
-  curveVertex(x+90,y+59);
-  curveVertex(x+75,y+61);
-  curveVertex(x+65,y+70);
-  curveVertex(x+52,y+69);
-  curveVertex(x+40,y+59);
-  curveVertex(x+45,y+44);
-  curveVertex(x+41,y+30);
-  endShape(CLOSE);
+function draw(){
 }
 
-  // fill('blue');
-  // stroke(1);
-  // beginShape();
-  // curveVertex(51,20);
-  // curveVertex(65,23);
-  // curveVertex(77,23);
-  // curveVertex(83,34);
-  // curveVertex(88,45);
-  // curveVertex(90,59);
-  // curveVertex(75,61);
-  // curveVertex(65,70);
-  // curveVertex(52,69);
-  // curveVertex(40,59);
-  // curveVertex(45,44);
-  // curveVertex(41,30);
-  // endShape(CLOSE);
-
+// COLOR CODE 
+// functions are yellow
+// variables are turcois
+// operators are white
+// pre-defined conditional loops are lilac
+// declarations are deep-blue
+// comments are deep-green
+// numbers are bright-green
+// ERROR color? / incomplete / unknown ...
