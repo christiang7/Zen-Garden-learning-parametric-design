6882029951141171590da7cedc0337b033aa536c
annameide <46717848+annameide@users.noreply.github.com>
2022-04-29 08:56:51 +0200

    code setup
    implementation of always needed code snippets from former sketch.js files

--- code/task-06/sketch.js
+++ code/task-06/sketch.js
@@ -1,11 +1,20 @@
-function preload(){
+// setting variables for canvas size
+const sketchHeight = 400;
+const sketchWidth = 400;
+
+function preload() {
   // preload assets
 }
 
+// canvas size is now described by the former set variables (NOT fixed values). syntax stays same
 function setup() {
-  createCanvas(400, 400, SVG);
+  createCanvas(sketchWidth, sketchHeight);
+  //background(100);
+  //frameRate(3); // only x times a second, so it is slower than the default
 }
 
 function draw() {
-  background('white');
+  //background('white');
+  //noLoop();
+  
 }
