207a232fbd28990acd379567c82ecf24b2dd7ad7
annameide <46717848+annameide@users.noreply.github.com>
2022-04-29 17:52:25 +0200

    finalization

--- code/task-06/sketch.js
+++ code/task-06/sketch.js
@@ -1,57 +1,52 @@
-// a circle is to be drawn on the canvas.
-// the x and y coordinates are being generated/constructed and stored in and by arrays 
+// seven circles are to be drawn on the canvas, each for one lottozahl  
 
 // setting variables for canvas size
 const sketchHeight = 400;
 const sketchWidth = 400;
 
-// we want 10 circles
+// we want 7 circles
 const circleCount = 7;
 
-const circleSize = 20;
+// syntax for circle is circle(x, y, d);
 
+const circleSize = 20; // = d
+
+// to draw the circles we already defined a constant d value, now we need x and y coordinates
+// the x and y coordinates are being stored in two arrays, one array for x and one arrray for y
 // arrays declaration
-// the length of the array is defined by the number of circles I want
-let xPosition = new Array(circleCount);
-let yPosition = new Array(circleCount);
+// the length of the array is defined by the number of circles
+let xPosition = new Array(circleCount); // array for x with 7 placeholders
+let yPosition = new Array(circleCount); // array for y with 7 placeholders
 
-// the x and y position arrays are being filled with values from 0-400 because of the canvas size
-xPosition[0] = 103;
-xPosition[1] = 117;
-xPosition[2] = 119;
-xPosition[3] = 125;
-xPosition[4] = 140;
-xPosition[5] = 146;
-xPosition[6] = 108;
-// xPosition[7] = 65;
-// xPosition[8] = 44;
-// xPosition[9] = 222;
+// the x and y position arrays are being filled with values from 0-max. Lottozahl + 100
+// xPosition[0] = 103; // fill placeholder 0 with value 103
+// xPosition[1] = 117;
+// xPosition[2] = 119;
+// xPosition[3] = 125;
+// xPosition[4] = 140;
+// xPosition[5] = 146;
+// xPosition[6] = 108;
 
+xPosition = [103, 117, 119, 125, 140, 146, 108];
 
-yPosition[0] = 113;
-yPosition[1] = 129;
-yPosition[2] = 132;
-yPosition[3] = 134;
-yPosition[4] = 138;
-yPosition[5] = 147;
-yPosition[6] = 106;
-// yPosition[7] = 35;
-// yPosition[8] = 24;
-// yPosition[9] = 122;
+// yPosition[0] = 113;
+// yPosition[1] = 129;
+// yPosition[2] = 132;
+// yPosition[3] = 134;
+// yPosition[4] = 138;
+// yPosition[5] = 147;
+// yPosition[6] = 106;
 
-function preload() {
-  // preload assets
-}
+yPosition = [113, 129, 132, 134, 138, 147, 106];
 
-// canvas size is now described by the former set variables (NOT fixed values). syntax stays same
 function setup() {
   createCanvas(sketchWidth, sketchHeight);
   background('yellow');
-  fill('black');
+  
+  fill('black'); 
   text('Die Lottozahlen + 100 ;)', 50, 20);
 
   fill('pink');
-
   // circle (xPosition[0], yPosition[0], circleSize);
   // circle (xPosition[1], yPosition[1], circleSize);
   // circle (xPosition[2], yPosition[2], circleSize);
@@ -59,12 +54,15 @@ function setup() {
   // circle (xPosition[4], yPosition[4], circleSize);
   // circle (xPosition[5], yPosition[5], circleSize);
   // circle (xPosition[6], yPosition[6], circleSize);
-  // circle (xPosition[7], yPosition[7], circleSize);
-  // circle (xPosition[8], yPosition[8], circleSize);
-  // circle (xPosition[9], yPosition[9], circleSize);
-
+  
+  
   for (let arrayIndex = 0; arrayIndex < circleCount; arrayIndex += 1) {
     circle(xPosition[arrayIndex], yPosition[arrayIndex], circleSize);
   }
 
 }
+
+// a for loop counts 0,1,2,3,4,... always one more (increment by 1) in order to execute commands
+// an array counts 0,1,2,3,4,... always one to the right (increment by 1) in order to store values
+// because they are similar in the counting aspect they can be combined effectively 
+// konkret: zählvariable der schleife kann mit indexvariable des array gleichgesetzt werden: hier arrayIndex 
