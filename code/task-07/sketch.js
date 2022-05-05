ec5b4f2387e2c33020ed83bc5522b0cdc8bdc577
annameide <46717848+annameide@users.noreply.github.com>
2022-05-05 12:43:57 +0200

    imported lys code version

--- code/task-07/sketch.js
+++ code/task-07/sketch.js
@@ -2,24 +2,15 @@ function preload(){
   // preload assets
 }
 
-var inc;
-
-
 
 function setup() {
-  let c = createCanvas(800, 800);
+  createCanvas(800, 800);
   background(255);
   noFill();
   t = 0;
 }
 
 function draw() {
-  
-
-  for(let counter=0; counter < 1000; counter++);{
-
-    fill('black');
-    text('EXHIBITION');
 
     stroke(230,20,20);
     strokeWeight(0.5);
@@ -30,8 +21,8 @@ function draw() {
       var x4 = width * noise(t,8,10,200);
       var y1 = height * noise(t,300);
       var y2 = height * noise(t+5, 500, 10);
-      var y3 = height * noise(t+6);
-      var y4 = height * noise(t+7);
+      var y3 = height * noise(t+6,777,30);
+      var y4 = height * noise(t+7, 100, 30);
 
   quad(x1,y1,x2,y2,x3,y3,x4,y4);
   t += 0.01;
@@ -44,11 +35,10 @@ function draw() {
     text(('Maxime Pattern'),50,250);
   textSize(40);
     text(('20.05.2022, 19 Uhr'),50,280);
+
+    //noLoop();
   }
   
   
   
 
- //noLoop();
-
-}
