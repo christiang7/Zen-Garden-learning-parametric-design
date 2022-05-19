c7e38a11ff8f9be36b38256b9389961471894934
annameide <46717848+annameide@users.noreply.github.com>
2022-05-19 10:56:46 +0200

    adding needed files from boilerplate

---
+++ code/zen-garden/sketch.js
@@ -0,0 +1,20 @@
+// setting variables for canvas size
+const sketchHeight = 400;
+const sketchWidth = 400;
+
+function preload() {
+  // preload assets
+}
+
+// canvas size is now described by the former set variables (NOT fixed values). syntax stays same
+function setup() {
+  createCanvas(sketchWidth, sketchHeight);
+  //background(100);
+  //frameRate(3); // only x times a second, so it is slower than the default
+}
+
+function draw() {
+  //background('white');
+  //noLoop();
+  
+}
